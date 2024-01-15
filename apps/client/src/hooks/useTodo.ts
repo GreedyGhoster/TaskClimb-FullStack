99e0a0aea392ee47c74138e9521c2b84ff6a57c4
IuanPR <103894461+IuanPR@users.noreply.github.com>
2024-01-15 21:49:41 +0300

    Add try/catch for useTodo

--- apps/client/src/hooks/useTodo.ts
+++ apps/client/src/hooks/useTodo.ts
@@ -26,41 +26,71 @@ function useTodoFunc() {
   });
 
   const getProjects = useCallback(async () => {
-    const res = await fetcher.get<IToDoProject[]>("/projects");
-    setProjects(res.data);
+    try {
+      const res = await fetcher.get<IToDoProject[]>("/projects");
+
+      if (res.status === 200) {
+        setProjects(res.data);
+      }
+    } catch (err) {
+      alert("Error: Please reload the page");
+    }
   }, []);
 
   const createProject = useCallback(async (title: string) => {
-    const data = { title: title };
-    const res = await fetcher.post("/projects", data);
-    const projectId = res.data.id;
-    const projectTitle = res.data.title;
+    try {
+      const data = { title: title };
+      const res = await fetcher.post("/projects", data);
+      const projectId = res.data.id;
+      const projectTitle = res.data.title;
 
-    setProjects((prev) => [{ id: projectId, title: projectTitle }, ...prev]);
+      if (res.status === 201) {
+        setProjects((prev) => [
+          { id: projectId, title: projectTitle },
+          ...prev,
+        ]);
+      }
+    } catch (err) {
+      alert("Error: Please reload the page");
+    }
   }, []);
 
   const deleteProject = useCallback(async (projectId: string) => {
-    await fetcher.delete(`/projects/${projectId}`);
-    setProjects((prev) => {
-      return prev.filter((project) => project.id !== projectId);
-    });
+    try {
+      const res = await fetcher.delete(`/projects/${projectId}`);
+
+      if (res.status === 204) {
+        setProjects((prev) => {
+          return prev.filter((project) => project.id !== projectId);
+        });
+      }
+    } catch (err) {
+      alert("Error: Please reload the page");
+    }
   }, []);
 
   const editProject = useCallback(
     async (projectId: string, newTitle: string) => {
-      const data = { title: newTitle };
-      setProjects((prev) => {
-        const next = [...prev];
-        const project = next.find((val) => val.id === projectId);
-        if (project) {
-          fetcher.patch(`/projects/${projectId}`, data);
-          project.title = newTitle;
-          return next;
-        } else {
-          console.log(`Проект ${projectId} не найден`);
-          return prev;
+      try {
+        const data = { title: newTitle };
+        const res = await fetcher.patch(`/projects/${projectId}`, data);
+
+        if (res.status === 200) {
+          setProjects((prev) => {
+            const next = [...prev];
+            const project = next.find((val) => val.id === projectId);
+            if (project) {
+              project.title = newTitle;
+              return next;
+            } else {
+              console.log(`Проект ${projectId} не найден`);
+              return prev;
+            }
+          });
         }
-      });
+      } catch (err) {
+        alert("Error: Please reload the page");
+      }
     },
     []
   );
@@ -75,8 +105,15 @@ function useTodoFunc() {
   );
 
   const getTasks = useCallback(async () => {
-    const res = await fetcher.get<IToDoTask[]>("/projects/tasks");
-    setTasks(res.data);
+    try {
+      const res = await fetcher.get<IToDoTask[]>("/projects/tasks");
+
+      if (res.status === 200) {
+        setTasks(res.data);
+      }
+    } catch (err) {
+      alert("Error: Please reload the page");
+    }
   }, []);
 
   const getTasksByProject = useCallback(
@@ -102,23 +139,30 @@ function useTodoFunc() {
 
   const addTask = useCallback(
     async (projectId: string, newTask: AddToDoTaskFormValues) => {
-      const data = {
-        title: newTask.title,
-        status: newTask.status,
-        description: newTask.description,
-      };
-      const res = await fetcher.post(`/projects/${projectId}`, data);
-      const taskId = await res.data.id;
-      setTasks((prev) => {
-        return [
-          {
-            id: taskId,
-            projectId: projectId,
-            ...newTask,
-          },
-          ...prev,
-        ];
-      });
+      try {
+        const data = {
+          title: newTask.title,
+          status: newTask.status,
+          description: newTask.description,
+        };
+        const res = await fetcher.post(`/projects/${projectId}`, data);
+        const taskId = await res.data.id;
+
+        if (res.status === 201) {
+          setTasks((prev) => {
+            return [
+              {
+                id: taskId,
+                projectId: projectId,
+                ...newTask,
+              },
+              ...prev,
+            ];
+          });
+        }
+      } catch (err) {
+        alert("Error: Please reload the page");
+      }
     },
     []
   );
@@ -138,72 +182,122 @@ function useTodoFunc() {
       projectId: string,
       editingTask: EditToDoTaskFormValues
     ) => {
-      const data = {
-        title: editingTask.title,
-        description: editingTask.description,
-      };
-      setTasks((prev) => {
-        const next = [...prev];
-        const task = next.find((val) => val.id === taskId);
-        if (task) {
-          fetcher.patch(`/projects/${projectId}/${taskId}`, data);
-          task.title = editingTask.title;
-          task.description = editingTask.description;
-          return next;
-        } else {
-          console.log(`Задача ${taskId} не найдена`);
-          return prev;
+      try {
+        const data = {
+          title: editingTask.title,
+          description: editingTask.description,
+        };
+        const res = await fetcher.patch(
+          `/projects/${projectId}/${taskId}`,
+          data
+        );
+
+        if (res.status === 200) {
+          setTasks((prev) => {
+            const next = [...prev];
+            const task = next.find((val) => val.id === taskId);
+            if (task) {
+              fetcher.patch(`/projects/${projectId}/${taskId}`, data);
+              task.title = editingTask.title;
+              task.description = editingTask.description;
+              return next;
+            } else {
+              console.log(`Задача ${taskId} не найдена`);
+              return prev;
+            }
+          });
         }
-      });
+      } catch (err) {
+        alert("Error: Please reload the page");
+      }
     },
     []
   );
 
   const deleteTask = useCallback(async (taskId: string, projectId: string) => {
-    await fetcher.delete(`/projects/${projectId}/${taskId}`);
-    setTasks((prev) => {
-      return prev.filter((task) => task.id !== taskId);
-    });
+    try {
+      const res = await fetcher.delete(`/projects/${projectId}/${taskId}`);
+
+      if (res.status === 204) {
+        setTasks((prev) => {
+          return prev.filter((task) => task.id !== taskId);
+        });
+      }
+    } catch (err) {
+      alert("Error: Please reload the page");
+    }
   }, []);
 
   const statusSwitcher = useCallback(
     async (taskId: string, projectId: string, statusName: string) => {
-      setTasks((prev) => {
-        const next = [...prev];
-        const task = next.find((val) => val.id === taskId);
+      try {
         const data = { status: statusName };
-        if (task) {
-          fetcher.patch(`/projects/${projectId}/${taskId}`, data);
-          task.status = statusName;
-          return next;
-        } else {
-          console.log(`Задача ${taskId} не найдена`);
-          return prev;
+        const res = await fetcher.patch(
+          `/projects/${projectId}/${taskId}`,
+          data
+        );
+
+        if (res.status === 200) {
+          setTasks((prev) => {
+            const next = [...prev];
+            const task = next.find((val) => val.id === taskId);
+
+            if (task) {
+              task.status = statusName;
+              return next;
+            } else {
+              console.log(`Задача ${taskId} не найдена`);
+              return prev;
+            }
+          });
         }
-      });
+      } catch (err) {
+        alert("Error: Please reload the page");
+      }
     },
     []
   );
 
   const getProfileData = useCallback(async () => {
-    const res = await fetcher.get("/users/me");
-    setProfileData(res.data);
+    try {
+      const res = await fetcher.get("/users/me");
+
+      if (res.status === 200) {
+        setProfileData(res.data);
+      }
+    } catch (err) {
+      alert("Error: Please reload the page");
+    }
   }, []);
 
   const deleteAccount = useCallback(async () => {
-    await fetcher.delete("/users/me");
-    setProfileData(undefined);
+    try {
+      const res = await fetcher.delete("/users/me");
+
+      if (res.status === 204) {
+        setProfileData(undefined);
+      }
+    } catch (err) {
+      alert("Error: Please reload the page");
+    }
   }, []);
 
   const updateAccountNickName = useCallback(
     async (newData: EditProfileNickName) => {
-      const data = {
-        nickName: newData.nickName,
-      };
+      try {
+        const data = {
+          nickName: newData.nickName,
+        };
 
-      const res = await fetcher.patch("/users/me/edit/nickname", data);
-      console.log(res.data);
-      // setProfileData((prev) => (prev!.nickName = .nickName));
+        const res = await fetcher.patch("/users/me/edit/nickname", data);
+        // setProfileData((prev) => (prev!.nickName = .nickName));
+
+        if (res.status === 200) {
+          console.log(res.data);
+        }
+      } catch (err) {
+        alert("Error: Please reload the page");
+      }
     },
     []
   );
